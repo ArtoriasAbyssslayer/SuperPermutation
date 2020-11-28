551d7b92a8c7549eb1de2608f6dbaea79a7eebd1
harryfil <harry.filis@yahoo.gr>
2020-11-28 18:32:07 +0200

    final form of superpermutation.py

--- SourceCode.py
+++ SourceCode.py
@@ -1,39 +1,118 @@
-#Import the lib perm from the package itertools in order to simplify the code
-import random
+import numpy as np
 from itertools import permutations
-#---the comments bellow reffer to another implementation of the project that i tried and failed
-#import numpy as np
-#import ndarray for the array of the initial vector we need
-#from numpy import ndarray
-#==================================================
-#1ST PART MAKE THE PERMUTATIONS
-#input from keyboard
-n = int(input('Enter an integer(it is the length of each permutation vector) :'))
-#create an empty array with fixed size
-#a = ndarray(n,int)
-#init_array = np.array(n,np.int64);
-#init vector input as a list
-init_vec = list(range(1,n+1))
-random.choice(init_vec)
-print(init_vec)
+import math
 
-#Get all permutations of the vector v = [n,n-1,n-2,.....,1]
-#and length n
-#the method permutations(initial vector, perm length) returns a list of vectors
-perm = permutations(init_vec,n);
-print("Permutation List is:")
-for i in list(perm):
-    print(i,sep = ",")
+N = 5
 
-#--------------------------------------------------------------------------
-#2ND PART make the smallest possible(length) vector with all permutation solutions and print it out
-#easy way to only concatenate all the permutation
-index = 0
-output_vector = list()
-for i in list(perm):
-    output_vector.insert(index, i)
-    index += 1
-print("The concatenated list is:")
-for x in range(len(output_vector)):
-    print(output_vector[x])
-#hard way to find the smallest vector with all the permutations
+class node():
+
+    def __init__(self, value=-1):
+        self.value = value
+        self.usedNums = []
+        self.children = []
+        self.parent = None
+        self.value = -2
+        self.level = 0
+
+
+def createPermutations(n):
+    l = list(permutations(range(1, n+1)))
+    print('Permutations list generated...')
+    ans = []
+    tempSum = ""
+    for a in l:
+        for i in a:
+            tempSum += str(i)
+        ans.append(tempSum)
+        tempSum = ""
+    return ans
+
+
+def find(L, target, cutDigits):
+    start = 0
+    end = len(L) - 1
+    while start <= end:
+        middle = (start + end) // 2
+        midpoint = L[middle][:cutDigits-2]
+        if midpoint > target:
+            end = middle - 1
+        elif midpoint < target:
+            start = middle + 1
+        else:
+            return [L[middle], middle]
+    return ['-1', '-1']
+
+
+def searchForNumInIterations(cutNum, l):
+    for i in range(len(l)):
+        item = l[i]
+        if cutNum in item:
+            return (item, i)
+    return ('-1', '-1')
+
+
+def calc(l):
+    finalString = ''
+    lastNum = l[0]
+    finalString = lastNum
+    del l[0]
+    # for i in l:
+    while len(l) > 0:
+        # Loop for each possible substring of 1234
+        for j in range(len(lastNum)):
+            # If lastNum is 0123, then the first cut_lastNum is 123
+            cut_lastNum = lastNum[j+1:]
+            # print('cut number: ' + str(cut_lastNum))
+            # We look for a number that starts with 123, 23, 3 etc..
+
+            # ans = searchForNumInIterations(cut_lastNum, l)
+            # ans = binarySearch(cut_lastNum, l)
+            ans = find(l, cut_lastNum, len(lastNum) - j+1)
+
+            # print(ans)
+            # If there is a suitable number found, the result is not -1
+            if ans[0] != '-1':
+                numToAppend = ans[0][len(cut_lastNum):]
+                finalString += (numToAppend)
+                lastNum = ans[0]
+                del l[ans[1]]
+                break
+    return finalString
+
+
+def removeList(a, b):
+    return list(set(a)-set(b))
+
+
+def verifyAnswer(l, answer):
+    # A = ahocorasick.Automaton()
+    # answer = '2123'
+    # A.add_word(answer, True)
+    # for a in l:
+    #     if A.match(str(a)) != True:
+    #         print(a)
+    #         return 'Failure'
+    # return "Pass"
+    # print('Verifying: ' + answer)
+    for a in l:
+        if a not in answer:
+            return 'Generated string is incorrect!'
+        return 'Generated string is verified!'
+
+
+def main():
+    # root1 = node()
+    print('Generating permutation...')
+    permutationList = createPermutations(N)
+    print('Permutations generated!')
+    finalString = calc(permutationList + [])
+    print('Answer calculated!')
+    print(finalString)
+    print('String length is: ' + str(len(finalString)))
+    # print(verifyAnswer(permutationList, finalString))
+
+
+if __name__ == "__main__":
+    main()
+
+# createPermutations(4)
